671ffa44ee685244a3ba4407319d01701843abe6
STICKER1998 <tzffin@gmial.com>
2022-10-31 09:08:46 +0100

    Upload Homework 2-V-1.3

--- Homework/week2/Homework2.2/docker_test.py
+++ Homework/week2/Homework2.2/docker_test.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# Read Data
 csv_data = pd.read_csv('coding-environment-exercise.csv')
+
+# Plot and Save figure
 x = np.array(csv_data)[:,0]
 y = np.array(csv_data)[:,1]
 fig = plt.figure(dpi=128, figsize=(10, 6))
