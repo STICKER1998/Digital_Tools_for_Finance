bb622fa4ff1c95cfc1f29ff86e8d76b66f77ceec
STICKER1998 <tzffin@gmial.com>
2022-12-19 01:12:53 +0100

    some files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for Digital Tools for Finance',
+    author='ZIFENG TANG / HAORAN ZHU / YUZHI MAO ',
+    license='MIT',
+)
